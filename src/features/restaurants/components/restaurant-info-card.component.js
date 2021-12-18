5c20d1573513d92d1a812cb13249bcfd03901cfb
Lee Macintyre <macintyrelee@gmail.com>
2021-12-18 10:21:59 +0000

    added theme into project

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,40 +1,57 @@
-
 import React from "react";
 import styled from "styled-components/native";
 import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
+import { fontSizes } from "../../../infastructure/theme/fonts";
+
+//pulled of Card to remove syntax error
+let {Cover} = Card
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`
 
 const RestaurantCard = styled(Card)`
   background-color: white;
 `;
 
-const RestaurantCardCover = styled(Card)`
-  padding: 20px;
+const RestaurantCardCover = styled(Cover)`
+  padding: ${(props) => props.theme.space[3]};
   background-color: white;
 `;
 
-const Title = styled(Text)`
-  padding: 16px;
+const Address = styled(Text)`
   color: ${(props) => props.theme.colors.ui.primary};
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
 `;
 
-export const RestaurantInfoCard = ({ restaurant = {} }) => {
-  const {
-    name = "Some Restaurant",
-    icon,
-    photos = [
-      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
-    ],
-    address = "100 some random street",
-    isOpenNow = true,
-    rating = 4,
-    isClosedTemporarily,
-  } = restaurant;
+const Title = styled(Text)`
+  color: ${(props) => props.theme.colors.ui.primary};
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
+`;
 
-  return (
-      <RestaurantCard elevation={5}>
-        <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
-        <Title>{name}</Title>
-      </RestaurantCard>
-  );
+export const RestaurantInfoCard = ({restaurant = {}}) => {
+    const {
+        name = "Some Restaurant",
+        icon,
+        photos = [
+            "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+        ],
+        address = "100 some random street",
+        isOpenNow = true,
+        rating = 4,
+        isClosedTemporarily,
+    } = restaurant;
+
+    return (
+        <RestaurantCard elevation={5}>
+            <RestaurantCardCover key={name} source={{uri: photos[0]}}/>
+            <Info>
+                <Title>{name}</Title>
+                <Address>{address}</Address>
+            </Info>
+        </RestaurantCard>
+    );
 };
