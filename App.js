29a57b0751ea40d32fd603895b31d0d51d3c9fbe
Lee Macintyre <macintyrelee@gmail.com>
2021-12-22 08:23:29 +0000

    added mock location and restaurant services

--- App.js
+++ App.js
@@ -1,16 +1,62 @@
-import {StatusBar as ExpoStatusBar} from 'expo-status-bar';
 import React from 'react';
-import {RestaurantsScreen} from './src/features/screens/restaurants.screen';
-import {theme} from './src/infastructure/theme';
-import {ThemeProvider} from 'styled-components/native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { RestaurantsScreen } from './src/features/screens/restaurants.screen';
+import { ThemeProvider } from 'styled-components/native';
+import { Text } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import { theme } from './src/infastructure/theme';
+import { SafeArea } from './src/components/utilities/safe-area.component';
+import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
+import { LocationContextProvider } from "./src/services/location/location.context";
+
+const Tab = createBottomTabNavigator();
+
+const TAB_ICON = {
+    Restaurants: "md-restaurant",
+    Map: "map",
+    Settings: "settings"
+}
+
+const createScreenOptions = ({route}) => {
+    const iconName = TAB_ICON[route.name]
+    return {
+        tabBarIcon: ({size, color}) => <Ionicons name={iconName} size={size} color={color}/>,
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "gray",
+        tabBarStyle: [
+            {
+                "display": "flex"
+            },
+            null
+        ]
+    }
+}
+
+const Settings = () => <SafeArea><Text>Settings</Text></SafeArea>
+const Map = () => <SafeArea><Text>Map</Text></SafeArea>
 
 export default function App() {
-  return (
-    <>
-      <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
-        <ExpoStatusBar style="auto" />
-      </ThemeProvider>
-    </>
-  );
+    return (
+        <>
+            <ThemeProvider theme={theme}>
+                <LocationContextProvider>
+                    <RestaurantsContextProvider>
+                        <NavigationContainer>
+                            <Tab.Navigator
+                                screenOptions={createScreenOptions}
+                            >
+                                <Tab.Screen name="Restaurants" component={RestaurantsScreen}/>
+                                <Tab.Screen name="Map" component={Map}/>
+                                <Tab.Screen name="Settings" component={Settings}/>
+                            </Tab.Navigator>
+                        </NavigationContainer>
+                    </RestaurantsContextProvider>
+                </LocationContextProvider>
+            </ThemeProvider>
+            <ExpoStatusBar style="auto"/>
+        </>
+    );
 }
--- src/features/screens/restaurants.screen.js
+++ src/features/screens/restaurants.screen.js
@@ -1,14 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
-import { SafeAreaView, StatusBar, View, FlatList } from 'react-native';
+import { View, FlatList } from 'react-native';
 import { Searchbar } from 'react-native-paper';
 import { RestaurantInfoCard } from '../restaurants/components/restaurant-info-card.component';
 import { Spacer } from "../../components/spacer/spacer.component";
-
-const SafeArea = styled(SafeAreaView)`
-  flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
-`;
+import { SafeArea } from "../../components/utilities/safe-area.component";
+import { RestaurantsContext } from "../../services/restaurants/restaurants.context";
+import { ActivityIndicator, Colors } from 'react-native-paper';
 
 const SearchContainer = styled(View)`
   padding: ${(props) => props.theme.space[3]};
@@ -19,27 +17,45 @@ const RestaurantList = styled(FlatList).attrs({
         padding: 16
     }
 })`
+`
 
+const LoadingContainer = styled(View)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
 `
 
 export const RestaurantsScreen = () => {
+    const {isLoading, error, restaurants} = useContext(RestaurantsContext);
     return (
-        <SafeArea>
-            <SearchContainer>
-                <Searchbar/>
-            </SearchContainer>
-            <RestaurantList
-                data={[{name: 1}, {name: 2}, {name: 3}, {name: 4}, {name: 5}, {name: 6}, {name: 7}, {name: 8}, {name: 9}, {name: 10}, {name: 11}, {name: 12}, {name: 13}, {name: 14}, {name: 15}, {name: 16}]}
-                renderItem={() =>
-                    <Spacer size={"large"} position={"bottom"}>
-                        <RestaurantInfoCard/>
-                    </Spacer>
+        <>
+            <SafeArea>
+                {isLoading &&
+                <LoadingContainer>
+                    <ActivityIndicator
+                        animating={true}
+                        color={Colors.limeA700}
+                        size={100}
+                        style={{marginLeft: -50, marginTop: -50}}/>
+
+                </LoadingContainer>
                 }
-                keyExtractor={(item) => item.name}
-                contentContainerStyle={{padding: 16}}
-            />
-            {/*<RestaurantInfoCard/>*/}
-        </SafeArea>
+                <SearchContainer>
+                    <Searchbar/>
+                </SearchContainer>
+                <RestaurantList
+                    data={restaurants}
+                    renderItem={({item}) => {
+                        return (
+                            <Spacer position="bottom" size="large">
+                                <RestaurantInfoCard restaurant={item}/>
+                            </Spacer>
+                        );
+                    }}
+                    keyExtractor={(item) => item.name}
+                />
+            </SafeArea>
+        </>
     );
 };
 
